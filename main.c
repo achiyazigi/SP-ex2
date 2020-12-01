e0f56206fcc3f69d435eec9a3fcc6b03b8044a9a
Achiya Zigler <73824224+achiyazigi@users.noreply.github.com>
2020-12-01 14:13:19 +0200

    updated output to match the moodle version

--- main.c
+++ main.c
@@ -1,83 +1,119 @@
 #include <stdio.h>
 #include "myBank.h"
+void printManu();
+void printManu(){
+	printf("Please choose a transaction type:\n\
+ O-Open Account\n\
+ B-Balance Inquiry\n\
+ D-Deposit\n\
+ W-Withdrawal\n\
+ C-Close Account\n\
+ I-Interest\n\
+ P-Print\n\
+ E-Exit\n");
+}
 
 int main(){
 	char c;
 	int an , i;
 	double amount;
 	
-	printf("Transaction type?: ");
+	printManu();
 	scanf(" %c",&c);
 	while(c!='O' && c!='B' && c!='D' && c!='W' && c!='C' && c!='I' && c!='P' && c!='E'){
 		printf("Wrong input\n");
-		printf("Transaction type?: ");
+		printManu();
 		scanf(" %c",&c);
 	}
 	while(c != 'E'){
 		if(c == 'O'){
-			printf("Initial deposit?: ");
-			scanf(" %lf",&amount);
-			open(amount);
+			printf("Please enter amount for deposit: ");
+			if(scanf(" %lf",&amount) == 0){
+				printf("Faild to read the amount\n");
+			}
+			else{
+				open(amount);
+			}
 		}
 		
 		if(c == 'B'){
-			printf("Account number?: ");
-			scanf(" %d",&an);
-			an -= 901;
-			if(an > -1 && an <50)
-				bank(an);
-			else printf("illegal account number\n");
+			printf("Please enter Account number: ");
+			if(scanf(" %d",&an) == 0){
+				printf("Falid to read the account number\n");
+			}
+			else{
+				an -= 901;
+				if(an > -1 && an <50)
+					bank(an);
+				else printf("illegal account number\n");
+			}
 		}
 		
 		if(c == 'D'){
-			printf("Account number?: ");
-			scanf(" %d",&an);
-			an-= 901;
-			if(an > -1 && an <50){
-				printf("amount?: ");
-				scanf(" %lf",&amount);
-				deposit(an, amount);			
+			printf("Please enter Account number: ");
+			if(scanf(" %d",&an) == 0){
+				printf("Falid to read the account number\n");
 			}
-			else printf("illegal account number\n");
+			else{
+				an-= 901;
+				if(an > -1 && an <50){
+					printf("amount?: ");
+					scanf(" %lf",&amount);
+					deposit(an, amount);			
+				}
+				else printf("illegal account number\n");
+			}
 		}
 		
 		if(c == 'W'){
-			printf("Account number?: ");
-			scanf(" %d",&an);
-			an-= 901;
-			if(an > -1 && an <50){
-				printf("amount?: ");
-				scanf(" %lf",&amount);
-				withdraw(an, amount);	
+			printf("Please enter account number: ");
+			if(scanf(" %d",&an) == 0){
+				printf("Falid to read the account number\n");
 			}
-			else printf("illegal account number\n");
+			else{
+				an-= 901;
+				if(an > -1 && an <50){
+					printf("Please enter the amount to withdraw: ");
+					scanf(" %lf",&amount);
+					withdraw(an, amount);	
+				}
+				else printf("illegal account number\n");
+			}
 		}
 		
 		if(c == 'C'){
-			printf("Account number?: ");
-			scanf(" %d",&an);
-			an-= 901;
-			if(an > -1 && an <50){	
-				close(an);
+			printf("Please enter Account number: ");
+			if(scanf(" %d",&an) == 0){
+				printf("Falid to read the account number\n");
 			}
-			else printf("illegal account number\n");
+			else{
+				an-= 901;
+				if(an > -1 && an <50){	
+					close(an);
+				}
+				else printf("illegal account number\n");
+			}
 		}
 		
 		if(c == 'I'){
-			printf("Interest rate?: ");
-			scanf(" %d",&i);
-			interest(i);
+			printf("Please enter interest rate: ");
+			if(scanf(" %d",&i) == 0){
+				printf("Faild to read the interest rate\n");
+			}
+			else{
+				interest(i);
+			}
 		}
 		
 		if(c == 'P'){
 			print();
 		}
 
-		printf("Transaction type?: ");
+		printManu();
 		scanf(" %c",&c);
 		while(c!='O' && c!='B' && c!='D' && c!='W' && c!='C' && c!='I' && c!='P' && c!='E'){
-			printf("Wrong input\n");
-			printf("Transaction type?: ");
+			printf("Invalid transaction type\n");
+			printManu();
 			scanf(" %c",&c);
 		}
 	}
--- myBank.c
+++ myBank.c
@@ -8,7 +8,7 @@ void open(double amount){
 		if(!accounts[0][i]){
 			accounts[0][i] = 1;
 			accounts[1][i] = amount;
-			printf("the account number is: %d\n",(i+901));
+			printf("New account number is: %d\n",(i+901));
 			break;
 		}
 	}
@@ -20,34 +20,34 @@ void open(double amount){
 
 void bank(int an){
 	if(accounts[0][an] ==1){
-		printf("the amount in this account is: %.2lf\n",accounts[1][an]);
+		printf("The balance of account number %d is: %.2lf\n",an,accounts[1][an]);
 	}
 	else{
-		printf("this account isn't open\n");
+		printf("This account is closed\n");
 	}
 }
 
 void deposit(int an, double amount){
 	if(accounts[0][an] ==1){
 		accounts[1][an] += amount;
-		printf("the new amount in this account is: %.2lf\n",accounts[1][an]);
+		printf("The new balance is: %.2lf\n",accounts[1][an]);
 	}
 	else{
-		printf("this account isn't open\n");
+		printf("This account is closed\n");
 	}
 }
 
 void withdraw(int an, double amount){
 	if(accounts[0][an] ==1){
 		if(accounts[1][an]-amount<0)
-			printf("not enough credit in this account\n");
+			printf("Cannot withdraw more than the balance\n");
 		else{
 			accounts[1][an] -= amount;
-			printf("the new amount in this account is: %.2lf\n",accounts[1][an]);
+			printf("The new balance is: %.2lf\n",accounts[1][an]);
 		}
 	}
 	else{
-		printf("this account isn't open\n");
+		printf("this account is closed\n");
 	}
 }
 
@@ -70,7 +70,7 @@ void interest(int ir){
 void print(){
 	for(int i=0; i<50; i++){
 		if(accounts[0][i] == 1)
-			printf("account: %d amount: %.2lf | ",(i + 901),accounts[1][i]);
+			printf("The balance of account number %d is: %.2lf\n",(i + 901),accounts[1][i]);
 	}
 	printf("\n");
 }
